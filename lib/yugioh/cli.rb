2fd95300946062e62cf3445cf2c99b70317c28df
Jas Spencer <jas.spencer414@gmail.com>
2019-11-21 15:54:54 +0000

    first commit

---
+++ lib/yugioh/cli.rb
@@ -0,0 +1,8 @@
+require "yugioh/cli/version"
+
+module Yugioh
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/yugioh/cli/version.rb
@@ -0,0 +1,5 @@
+module Yugioh
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
